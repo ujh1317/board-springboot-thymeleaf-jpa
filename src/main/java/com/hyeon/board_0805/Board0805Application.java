f44ebcf28c95c5a9d4cd209bdfaae2c19cdb9bed
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-08-05 22:15:00 +0900

    init

---
+++ src/main/java/com/hyeon/board_0805/Board0805Application.java
@@ -0,0 +1,13 @@
+package com.hyeon.board_0805;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board0805Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board0805Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyeon/board_0805/Board0805ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeon.board_0805;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board0805ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
