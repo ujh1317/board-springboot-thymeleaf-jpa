ba6ffbaa81774ea6183100ee923906a693d7c5fe
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-08-08 11:19:48 +0900

    board write

--- src/main/java/com/hyeon/board_0805/controller/BoardController.java
+++ src/main/java/com/hyeon/board_0805/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +24,17 @@ public class BoardController {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards",boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String submit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
