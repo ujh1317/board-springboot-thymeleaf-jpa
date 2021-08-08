5cf974cfeab06ee469883805c0228cde7f3b1e44
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-08-08 20:51:05 +0900

    page

--- src/main/java/com/hyeon/board_0805/controller/BoardController.java
+++ src/main/java/com/hyeon/board_0805/controller/BoardController.java
@@ -4,6 +4,10 @@ import com.hyeon.board_0805.model.Board;
 import com.hyeon.board_0805.repository.BoardRepository;
 import com.hyeon.board_0805.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,8 +27,13 @@ public class BoardController {
     private BoardValidator boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 5) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards",boards);
         return "board/list";
     }
