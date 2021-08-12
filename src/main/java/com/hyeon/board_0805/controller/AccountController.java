4644df192880376016a8786445ea5d1675cd9c8b
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-08-12 23:16:36 +0900

    login

---
+++ src/main/java/com/hyeon/board_0805/controller/AccountController.java
@@ -0,0 +1,22 @@
+package com.hyeon.board_0805.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "account/login";
+    }
+
+//    @PostMapping("/register")
+//    public String login() {
+//        return "account/login";
+//    }
+
+}
---
+++ src/main/java/com/hyeon/board_0805/model/User.java
@@ -0,0 +1,22 @@
+package com.hyeon.board_0805.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+}
